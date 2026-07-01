14fc03f2cbe036164ab62f45429e197b0a2cb876
alimrahmat-dev <alimrahmat.dev@gmail.com>
2026-07-02 06:27:23 +0700

    yang kedua

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function authtentication(Request $request)
+    {
+        $validations = $request->validate([
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+
+
+        $user = User::where('email', $validations['email'])->first();
+
+        if (!$user || !Hash::check($validations['password'], $user->password)) {
+
+            return response()->json([
+                'message' => 'password dan email tidak sesuai'
+            ], 401);
+        }
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+        return response()->json([
+            'message' => 'Login Berhasil',
+            'auth_token' => $token
+        ]);
+    }
+}
